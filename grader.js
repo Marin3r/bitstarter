339023123aae8c7cab262744b51c578b6baaa9d1
Vasco de Gouveia <vascodegouveia@yahoo.co.uk>
2013-07-09 19:00:10 +0000

    Updated Grader

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -31,22 +32,23 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); 
+	// http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
+//only use this outside the restler commands (sync mode)
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(fs.readFileSync(htmlfile));   
 };
 
 var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+    return JSON.parse(fs.readFileSync(checksfile)).sort();  
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+var checkHtmlFile = function(cheeriofile, checks) {
+    $ = cheeriofile;
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
@@ -65,10 +67,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Path to url') 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checks=loadChecks(program.checks);
+    if (program.url){
+	// use restler to get html file from url 
+	rest.get(program.url).on('complete', function(data) {
+	    var cheeriofile=cheerio.load(data);
+	    var checkJson = checkHtmlFile(cheeriofile,checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	});
+    } else {
+	// use html file
+	var cheeriofile = cheerioHtmlFile(program.file);
+	var checkJson = checkHtmlFile(cheeriofile,checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
